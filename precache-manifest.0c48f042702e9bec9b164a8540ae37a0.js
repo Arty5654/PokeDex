7367beb02e38b876e26a3f8a7e6e680073d41dd0
Arty Avetissian <itsarty5654@gmail.com>
2023-06-06 00:34:26 -0400

    Updates

---
+++ precache-manifest.0c48f042702e9bec9b164a8540ae37a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc725d71b309bf3a2f2d",
+    "url": "/PokeDex/static/css/main.28cd563e.chunk.css"
+  },
+  {
+    "revision": "bc725d71b309bf3a2f2d",
+    "url": "/PokeDex/static/js/main.79831f5f.chunk.js"
+  },
+  {
+    "revision": "a7900f230fa8bac834eb",
+    "url": "/PokeDex/static/js/runtime~main.fe321f07.js"
+  },
+  {
+    "revision": "7f811f56110fd6d2983a",
+    "url": "/PokeDex/static/js/2.5d4d13be.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/PokeDex/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "a18331afc8b021758a826f345b740cdd",
+    "url": "/PokeDex/index.html"
+  }
+];
